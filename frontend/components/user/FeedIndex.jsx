78db7bc011474489c5ab321b2b19cd7456ee3659
dawnington <dawnington@gmail.com>
2016-07-05 11:44:03 -0700

    edited seeds to make image sizes smaller

--- frontend/components/user/FeedIndex.jsx
+++ frontend/components/user/FeedIndex.jsx
@@ -3,10 +3,11 @@ const PinActions = require('../../actions/PinActions');
 const PinStore = require('../../stores/PinStore');
 const PinIndexItem = require('../pin/PinIndexItem');
 const React = require('react');
+const InfiniteScroll = require('react-infinite-scroller');
 
 const FeedIndex = React.createClass({
   getInitialState() {
-    return { pins: PinStore.all() };
+    return { pins: PinStore.all(), shownPins: PinStore.all().slice(0, 15) };
   },
   componentDidMount() {
     this.pinListener = PinStore.addListener(this.onChange);
@@ -20,17 +21,31 @@ const FeedIndex = React.createClass({
     PinActions.emptyStore();
   },
   onChange() {
-    this.setState({ pins: PinStore.all() });
+    this.setState({ pins: PinStore.all(), shownPins: PinStore.all().slice(0, 15) });
+  },
+  loadFunc(pageNum) {
+    const allPins = PinStore.all();
+    this.setState({ shownPins: allPins.slice(0, 15 * (pageNum + 1)) });
   },
   render() {
+    let infiniteScroll = '';
+    if (this.state.pins.length > 0) {
+      infiniteScroll = (
+        <InfiniteScroll pageStart={0} loadMore={this.loadFunc} hasMore={this.state.pins.length > this.state.shownPins.length} >
+          <Masonry className="pin-index">
+            {
+              this.state.shownPins.map(pin =>
+                <PinIndexItem pin={pin} key={pin.pinning_id} />
+              )
+            }
+          </Masonry>
+        </InfiniteScroll>
+			);
+    }
     return (
-      <Masonry className="pin-index">
-        {
-          this.state.pins.map(pin =>
-            <PinIndexItem pin={pin} key={pin.pinning_id} />
-          )
-        }
-      </Masonry>
+      <div>
+        {infiniteScroll}
+      </div>
     );
   },
 });
